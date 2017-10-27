750872f588fafda262fad9ac2137ef26f56d323d
Niklas Spanring <niklas.spanring@gmx.at>
2017-10-27 13:14:13 +0200

    Update: Optimation of the Animation

--- js/animation.js
+++ js/animation.js
@@ -5,7 +5,6 @@ init();
 animate();
 
 function init() {
-
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -27,21 +26,13 @@ function init() {
   var light = new THREE.PointLight( 0xffffff, 1 );
   camera.add( light );
 
-  var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
-
-  var meshMaterial = new THREE.MeshLambertMaterial( {
-    color: 0xffffff,
-    opacity: 0.5,
-    transparent: true
-  } );
-
   group = new THREE.Group();
   scene.add( group );
 
+  var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
   var wireframe = new THREE.WireframeGeometry( geometry );
   var mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
   var line = new THREE.LineSegments( wireframe, mat );
-  line.color = 0xffffff;
   line.material.depthTest = false;
   line.material.opacity = 0.5;
   line.material.transparent = true;
@@ -50,36 +41,29 @@ function init() {
   for (var x = 10; x > 0; x--) {
     for (var y = 10; y > 0; y--) {
       line2 = line.clone();
-      line2.position.x = x*20;
-      line2.position.y = y*20;
+      line2.position.x = x*30;
+      line2.position.y = y*30;
       scene.add( line2 );
     }
   }
 
   group.add( line );
 
-  //
-
   window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function animate() {
-
   requestAnimationFrame( animate );
 
   group.rotation.x += 0.00;
   group.rotation.y += 0.01;
 
   renderer.render( scene, camera );
-
 }
